b58ba1e40e26f1bd09af1b8f2a272448508d836e
chuhann <chuhan8099@hotmail.com>
2020-09-09 01:23:31 +0800

    Handle error exceptions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -18,13 +18,104 @@ public class Duke {
     /**
      * Mark item in the inventory as checked
      *
-     * @param index index of item in inventory
+     * @param line line of command string
      * @param inventory inventory of all items
      */
-    public static void checkInventory(int index, ArrayList<Task> inventory){
-        inventory.get(index).setIsChecked(true);
-        System.out.println("Nice! I've marked this task as done: ");
-        System.out.println(inventory.get(index)+"\n");
+    public static void checkInventory(String line, ArrayList<Task> inventory){
+        try {
+            line = line.replace("done ","");
+            int index = Integer.parseInt(line) -1;
+            inventory.get(index).setIsChecked(true);
+            System.out.println("Nice! I've marked this task as done: ");
+            System.out.println(inventory.get(index)+"\n");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Item is not in List ! Please try with another valid index");
+        }catch(NumberFormatException e){
+            System.out.println("Please enter a valid number !");
+        }
+    }
+
+    /**
+     * Add todo item into inventory
+     *
+     * @param line line of command string
+     * @param inventory inventory of all items
+     */
+    public static void todoTask(String line, ArrayList<Task> inventory){
+        try {
+            // Add items to Todo list
+            line = line.replace("todo","").trim();
+            if(line.equals("")){
+                throw new TaskException();
+            }
+            Todo todo = new Todo(line);
+            inventory.add(todo);
+            System.out.println("Got it. I've added this task: ");
+            System.out.println(todo);
+            System.out.println("Now you have "+inventory.size()+" tasks in the list.\n");
+        } catch (TaskException e) {
+            System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
+        }
+    }
+
+    /**
+     * Add deadline task to inventory
+     *
+     * @param line line of command string
+     * @param inventory inventory of all items
+     */
+    public static void deadlineTask(String line, ArrayList<Task> inventory){
+        try {
+            String by;
+            line = line.replace("deadline","").trim();
+
+            if(line.equals("")){
+                throw new TaskException();
+            }
+
+            by = line.substring(line.lastIndexOf("/by") + 1);
+            line = line.substring(0,line.lastIndexOf("/by "));
+            by = by.replace("by ","");
+
+            Deadline deadline = new Deadline(line, by);
+            inventory.add(deadline);
+            System.out.println("Got it. I've added this task: ");
+            System.out.println(deadline);
+            System.out.println("Now you have "+inventory.size()+" tasks in the list.\n");
+        } catch (TaskException e) {
+            System.out.println("☹ OOPS!!! The description of a deadline cannot be empty.");
+        }catch (StringIndexOutOfBoundsException e){
+            System.out.println("Please use '/by' to specify date");
+        }
+    }
+
+    /**
+     * Add event task to inventory
+     *
+     * @param line line of command string
+     * @param inventory inventory of all items
+     */
+    public static void eventTask(String line, ArrayList<Task> inventory){
+        try {
+            String at;
+            line = line.replace("event","").trim();
+            if(line.equals("")){
+                throw new TaskException();
+            }
+            at = line.substring(line.lastIndexOf("/at ") + 1);
+            line = line.substring(0,line.lastIndexOf("/at "));
+            at = at.replace("at ","");
+
+            Event event = new Event(line, at);
+            inventory.add(event);
+            System.out.println("Got it. I've added this task: ");
+            System.out.println(event);
+            System.out.println("Now you have "+inventory.size()+" tasks in the list.\n");
+        } catch (TaskException e) {
+            System.out.println("☹ OOPS!!! The description of a event cannot be empty.");
+        } catch (StringIndexOutOfBoundsException e){
+            System.out.println("Please use '/at' to specify date");
+        }
     }
 
     public static void main(String[] args) {
@@ -36,63 +127,37 @@ public class Duke {
         System.out.println(intro);
         Scanner in = new Scanner(System.in);
         String line = in.nextLine();
-        String by;
-        String at;
 
         // Takes in user input until "bye" is entered by user
         while (!line.equals("bye")){
-            // List inventory
-            if (line.equals("list")){
-                listInventory(inventory);
-            }else if(line.startsWith("done ")){
-                // check items
-                line = line.replace("done ","");
-                try{
-                    int index = Integer.parseInt(line) -1;
-                    checkInventory(index, inventory);
-                }catch(Exception e){
-                    // Catch exception
-                    System.out.println("Opps Error");
+            try {
+                if (line.equals("list")){
+                    // List inventory
+                    listInventory(inventory);
+                }else if(line.startsWith("done")){
+                    // check items
+                    checkInventory(line, inventory);
+                } else if(line.startsWith("todo")){
+                    // Add items to Todo list
+                    todoTask(line, inventory);
+                } else if (line.startsWith("deadline")){
+                    //Add items to Deadline list
+                    deadlineTask(line, inventory);
+                } else if (line.startsWith("event")){
+                    //Add items to Event list
+                    eventTask(line, inventory);
+                }else{
+                    //Invalid command
+                    throw new DukeException();
                 }
-            } else if(line.startsWith("todo ")){
-                // Add items to Todo list
-                line = line.replace("todo ","");
 
-                Todo todo = new Todo(line);
-                inventory.add(todo);
-                System.out.println("Got it. I've added this task: ");
-                System.out.println(todo);
-                System.out.println("Now you have "+inventory.size()+" tasks in the list.\n");
-            } else if (line.startsWith("deadline ")){
-                //Add items to Deadline list
-                line = line.replace("deadline ","");
-                by = line.substring(line.lastIndexOf("/by") + 1);
-                line = line.substring(0,line.lastIndexOf("/by "));
-                by = by.replace("by ","");
-
-                Deadline deadline = new Deadline(line, by);
-                inventory.add(deadline);
-                System.out.println("Got it. I've added this task: ");
-                System.out.println(deadline);
-                System.out.println("Now you have "+inventory.size()+" tasks in the list.\n");
-            } else if (line.startsWith("event ")){
-                //Add items to Event list
-                line = line.replace("event ","");
-                at = line.substring(line.lastIndexOf("/at ") + 1);
-                line = line.substring(0,line.lastIndexOf("/at "));
-                at = at.replace("at ","");
-
-                Event event = new Event(line, at);
-                inventory.add(event);
-                System.out.println("Got it. I've added this task: ");
-                System.out.println(event);
-                System.out.println("Now you have "+inventory.size()+" tasks in the list.\n");
+            } catch (DukeException e) {
+                System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
             }
-
             line = in.nextLine();
         }
 
-        // Exit message
+        // Exit message todo “Answer CS2113 tutorial questions” todo “Answer CS2113 tutorial questions”
         if(line.equals("bye")){
             System.out.println(bye);
         }
